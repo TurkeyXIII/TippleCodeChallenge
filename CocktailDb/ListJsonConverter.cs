ef2d1d44a3f2b2a2225e3bdb9e28898e9a8c1319
Phill <boyackp@gmail.com>
2020-03-29 13:31:47 +1100

    Partial attempt at custom Json deserializer

---
+++ CocktailDb/ListJsonConverter.cs
@@ -0,0 +1,40 @@
+﻿using CocktailDb.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CocktailDb
+{
+    public class ListJsonConverter : JsonConverter
+    {
+        private Regex partOfAListRegex = new Regex(@"^(?<ListName>.*)[0-9]+$");
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Cocktail);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType != JsonToken.PropertyName)
+                return serializer.Deserialize(reader, objectType);
+
+            var regexMatch = partOfAListRegex.Match(reader.Value as string);
+
+            if (!regexMatch.Success)
+                return serializer.Deserialize(reader, objectType);
+
+            //TODO: figure out how to do the rest!?
+            return serializer.Deserialize(reader, objectType);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool CanWrite => false;
+    }
+}
---
+++ Tests/CocktailDb/ListJsonConverterTests.cs
@@ -0,0 +1,67 @@
+﻿using CocktailDb;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests.CocktailDb
+{
+    [TestClass]
+    public class ListJsonConverterTests
+    {
+        [TestMethod]
+        public void Deserialize_NormalString()
+        {
+            var input = new InputObject
+            {
+                NormalString = "abc",
+            };
+            string json = JsonConvert.SerializeObject(input);
+
+            var result = JsonConvert.DeserializeObject<ResultObject>(json, new ListJsonConverter());
+
+            Assert.AreEqual(input.NormalString, result.NormalString);
+        }
+
+        [TestMethod]
+        public void Deserialize_StringInListOfOne()
+        {
+            var input = new InputObject
+            {
+                ListOfOneString1 = "abc",
+            };
+            string json = JsonConvert.SerializeObject(input);
+
+            var result = JsonConvert.DeserializeObject<ResultObject>(json, new ListJsonConverter());
+
+            Assert.AreEqual(1, result.ListOfOneString.Count);
+            Assert.AreEqual(input.ListOfOneString1, result.ListOfOneString.Single());
+        }
+
+        [TestMethod]
+        public void Deserialize_StringInListOfMany()
+        {
+            Assert.Inconclusive();
+        }
+    }
+
+    internal class InputObject
+    {
+        public string NormalString { get; set; }
+
+        public string ListOfOneString1 { get; set; }
+
+        public string ListOfManyStrings1 { get; set; }
+        public string ListOfManyStrings2 { get; set; }
+        public string ListOfManyStrings3 { get; set; }
+    }
+
+    internal class ResultObject
+    {
+        public string NormalString { get; set; }
+        public List<string> ListOfOneString { get; set; }
+        public List<string> ListOfManyStrings { get; set; }
+    }
+}
