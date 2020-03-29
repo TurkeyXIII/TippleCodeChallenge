4f3556ef37d16ea7d433e3bb5de1d2d5ff339089
Phill <boyackp@gmail.com>
2020-03-29 14:48:13 +1100

    Kind-of half working

--- CocktailDb/ListJsonConverter.cs
+++ CocktailDb/ListJsonConverter.cs
@@ -1,9 +1,12 @@
 ﻿using CocktailDb.Dtos;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CocktailDb
 {
@@ -13,21 +16,32 @@ namespace CocktailDb
 
         public override bool CanConvert(Type objectType)
         {
-            return objectType == typeof(Cocktail);
+            return true;
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            if (reader.TokenType != JsonToken.PropertyName)
-                return serializer.Deserialize(reader, objectType);
+            if (reader.TokenType == JsonToken.StartObject)
+            {
+                JObject item = JObject.Load(reader);
 
-            var regexMatch = partOfAListRegex.Match(reader.Value as string);
+                var propertyNames = item.Properties().Select(p => p.Name);
 
-            if (!regexMatch.Success)
-                return serializer.Deserialize(reader, objectType);
+                var propertyNamesGroupedByListName = propertyNames.Where(n => partOfAListRegex.IsMatch(n))
+                    .GroupBy(n => partOfAListRegex.Match(n).Groups["ListName"].Value, n => n);
 
-            //TODO: figure out how to do the rest!?
-            return serializer.Deserialize(reader, objectType);
+                foreach (IGrouping<string, string> group in propertyNamesGroupedByListName)
+                {
+                    List<string> propertyValues = group.ToList();
+                    string propertyName = group.Key;
+
+                    item.Add(propertyName, JToken.FromObject(propertyValues));
+                }
+
+                return item.ToObject(objectType, JsonSerializer.CreateDefault());
+            }
+
+            throw new Exception();
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
